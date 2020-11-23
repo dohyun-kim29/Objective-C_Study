a7bcad17d39ba0fc8d8dfc613963f613433b47ff
DohyunKIm <rlaehgus1229@gmail.com>
2020-11-23 19:47:33 +0900

    [Add] Basic.m

--- Objective-C_Study/Objective-C_Study/Basic.m
+++ Objective-C_Study/Objective-C_Study/Basic.m
@@ -11,6 +11,13 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        //주석이당
+        
+        /*
+         이거도 주석이당
+         */
+        
+        NSLog(@"안녕하새오 도혀니애오");
     }
     return 0;
 }
