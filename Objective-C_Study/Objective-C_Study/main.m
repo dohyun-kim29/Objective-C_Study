b8cd6549f64621ae26fca1a3408497c88f591bea
DohyunKIm <rlaehgus1229@gmail.com>
2020-11-23 19:23:42 +0900

    [Add] XcodeProject

---
+++ Objective-C_Study/Objective-C_Study/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C_Study
+//
+//  Created by DohyunKim on 2020/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
